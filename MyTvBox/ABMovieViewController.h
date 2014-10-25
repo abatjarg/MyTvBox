ad028f0ab33228d3ffb0271fd40b0554e89b1143
Ariunjargal Batjargal <abatjarg@icloud.com>
2014-10-25 15:34:19 -0400

    Setup the TabBar and added Movie view controller

---
+++ MyTvBox/ABMovieViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABMovieViewController.h
+//  MyTvBox
+//
+//  Created by Ariunjargal on 10/25/14.
+//  Copyright (c) 2014 Ariunjargal Batjargal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABMovieViewController : UIViewController
+
+@end
