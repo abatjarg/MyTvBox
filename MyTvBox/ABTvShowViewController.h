5de7191c7bc320646b2a964268652b16719904be
Ariunjargal Batjargal <abatjarg@icloud.com>
2014-11-12 21:59:09 -0500

    Added tv show view controller

---
+++ MyTvBox/ABTvShowViewController.h
@@ -0,0 +1,13 @@
+//
+//  ABTvShowViewController.h
+//  MyTvBox
+//
+//  Created by Ariunjargal on 11/12/14.
+//  Copyright (c) 2014 Ariunjargal Batjargal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABTvShowViewController : UIViewController
+
+@end
