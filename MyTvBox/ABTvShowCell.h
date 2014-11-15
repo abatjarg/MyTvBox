4a98f2cb404ad5d8e46d668b0e13d8eddd538a0e
Ariunjargal Batjargal <abatjarg@icloud.com>
2014-11-14 22:57:23 -0500

    Added tv show cell

---
+++ MyTvBox/ABTvShowCell.h
@@ -0,0 +1,13 @@
+//
+//  ABTvShowCell.h
+//  MyTvBox
+//
+//  Created by Ariunjargal on 11/14/14.
+//  Copyright (c) 2014 Ariunjargal Batjargal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABTvShowCell : UICollectionViewCell
+
+@end
---
+++ MyTvBox/ABTvShowCell.m
@@ -0,0 +1,31 @@
+//
+//  ABTvShowCell.m
+//  MyTvBox
+//
+//  Created by Ariunjargal on 11/14/14.
+//  Copyright (c) 2014 Ariunjargal Batjargal. All rights reserved.
+//
+
+#import "ABTvShowCell.h"
+
+@implementation ABTvShowCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
